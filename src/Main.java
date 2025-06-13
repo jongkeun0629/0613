e7bd1a24fe67b2d93a0d0b5d50723eb41971dd51
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-13 14:37:28 +0900

    Interface

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,18 @@
-abstract class Shape{
-    abstract double area();
+interface Animal{
+    void makeSound();
 }
 
-class Circle extends Shape{
-    private int radius;
-
-    public Circle(int radius){
-        this.radius = radius;
-    }
-
+class Dog implements Animal {
     @Override
-    public double area(){
-        return radius * radius * Math.PI;
+    public void makeSound(){
+        System.out.println("멍멍");
     }
-
-//    public void area(){
-//        double result =  radius * radius * Math.PI;
-//        System.out.println("Circle Area : " + result);
-//    }
-}
-
-class Rectangle extends Shape{
-    private int width, height;
-
-    public Rectangle(int width, int height){
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public double area() {
-        return width * height;
-    }
-
-//    public void area(){
-//        double result =  width * height
-//        System.out.println("Rectangle Area : " + result);
-//    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
+        Animal dog = new Dog();
 
-        shapes[0] = new Circle(3);
-        shapes[1] = new Rectangle(4, 5);
-
-        for (Shape shape : shapes) {
-            System.out.println(shape.area());
-            // shape.area();
-        }
-
+        dog.makeSound();
     }
 }
-
