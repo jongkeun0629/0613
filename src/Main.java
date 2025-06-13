29f0876a64a7090b7bdb901b8f7ec0b85d33d1d1
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-13 17:14:56 +0900

    CPU(Load,Save)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,43 @@
-abstract class Animal{
-    abstract void makeSound();
+class CPU{
+    public int register = 0;
+    public int[] memory = {5, 10, 0};
 
-    public void sleep(){
-        System.out.println("드르렁");
-    }
-}
+    private static final int LOAD = 1;
+    private static final int SAVE = 2;
+    private static final int ADD = 3;
+    private static final int SUB = 4;
+    private static final int HALT = 5;
 
-class Dog extends Animal{
-    @Override
-    void makeSound(){
-        System.out.println("멍멍");
+    private int[][] program = {
+            { LOAD, 0 },         // register = memory[0]
+            { ADD, 1 },         // register = register + memory[1]
+            { SAVE, 2 },         // memory[2] = register
+            { HALT, 0 }         // stop program execution
+    };
+
+    public void executeProgram(){
+        int pc = 0;
+
+        while(true){
+            int opcode = program[pc][0];
+            int operand = program[pc][1];
+
+            switch (opcode){
+                case LOAD:
+                    register = memory[operand];
+                    System.out.println("LOAD : Loading memory[" + operand + "] (" + memory[0] + ") into register.");
+                    break;
+                case SAVE:
+                    memory[operand] = register;
+                    System.out.println("SAVE : Saving memory[" + operand + "] (" + memory[0] + ") from register.");
+                    break;
+            }
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog();
 
-        dog.makeSound();
-        dog.sleep();
     }
 }
