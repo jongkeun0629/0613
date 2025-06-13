9a14d812e6ee17f11b08b6a0195afcdf9b8e5c1d
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-13 11:24:34 +0900

    getter/constructor

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,19 @@
-class Person{
-    private String name;
-    private int age;
+class ReadOnlyStudent {
+    private final int score;
 
-    public String getName(){
-        return name;
+    public ReadOnlyStudent(int score) {
+        this.score = score;
     }
 
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public int getAge(){
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        if (age < 18) {
-            System.out.println("성인만 가입 가능.");
-        } else {
-            this.age = age;
-        }
+    public int getScore(){
+        return score;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
-
-        person.setName("종근");
-        person.setAge(26);
-
-        System.out.println(person.getName());
-        System.out.println(person.getAge());
+        ReadOnlyStudent ros = new ReadOnlyStudent(98);
+        System.out.println(ros.getScore());
     }
 }
 
